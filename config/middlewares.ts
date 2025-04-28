282f20c957ffface94201cf7b294676b40854d6e
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-28 13:33:43 +0530

    cors changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,16 @@
-export default [
+module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['https://strapi-frontend-ruddy.vercel.app'],  // Allow your frontend domain
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+      allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
