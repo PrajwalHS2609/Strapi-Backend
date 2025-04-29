35b7576fbd9c956af3516b3be6340b913f9cf03d
Prajwal H S <prajwalhs4india@gmail.com>
2025-04-29 12:02:21 +0530

    changes

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,20 +1,32 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: ['https://strapi-frontend-ruddy.vercel.app'],  // Allow your frontend domain
-      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-      allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-    },
-  },
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+// module.exports = [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       enabled: true,
+//       origin: ['https://strapi-frontend-ruddy.vercel.app'],  // Allow your frontend domain
+//       methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
+//       allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+//     },
+//   },
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+export default [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
